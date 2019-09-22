cf9c57e97210fcce616e575291fe8da0bf75377c
unknown <huangxinping@cyou-inc.com>
2019-09-22 17:37:01 +0800

    config

---
+++ Config/config-server/src/test/java/com/longwen/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.longwen.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
